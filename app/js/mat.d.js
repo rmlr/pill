fa1b0fde3d25215b2b0dcb9b2b9173879c251e73
AFW Gestion <rodolphe@larochelle.tv>
2016-02-05 12:21:23 +0100

    Story 12: modify

--- app/js/mat.d.js
+++ app/js/mat.d.js
@@ -1,54 +1,54 @@
-var canvas, ctx, columns, mainInterval, whiteFontInterval, soundKeypress, soundModem, MatrixMainWrapper, introParam;
+var canvas, ctx, columns, mainInt, whiteFontInt, sdKeypress, sdModem, matMainWrapper, intParam;
 var letters = "abcdefghijklmnopqrstuwxyz1234567890";
 var fontSize = 25;
-var keypressInterval = 200;
-var canvasLoaded = false;
-var timeoutParam = 500;
-var letterPos = [];
+var kpressInt = 200;
+var canvasLoad = false;
+var timeoutPar = 500;
+var lettersPos = [];
 var prev = [];
 var timeouts = [];
-var bodyLoaded = false;
+var bodyLoad = false;
 
 window.addEventListener("resize", onCanvasResize);
 
 window.onload = function() {
-	if(introParam == 'write' || introParam == 'read') {var dir = '../'} else {var dir = ''}
-	MatrixMainWrapper = document.getElementsByClassName('Matrix-mainWrapper')[0];
-	MatrixMainWrapper.style.display = 'none';
+	if(intParam == 'write' || intParam == 'read') {var dir = '../'} else {var dir = ''}
+	matMainWrapper = document.getElementsByClassName('Mat-mainWrapper')[0];
+	matMainWrapper.style.display = 'none';
 	var headerInnerStyle = document.createElement('style');
-	headerInnerStyle.appendChild(document.createTextNode('@font-face {font-family: "matrixFont";src: url("' + dir + 'fonts/matrix.ttf");}'));
-	headerInnerStyle.appendChild(document.createTextNode('.Matrix-mainWrapper {position: relative;}'));
+	headerInnerStyle.appendChild(document.createTextNode('@font-face {font-family: "matrixFont";src: url("' + dir + 'fonts/matrixFont.ttf");}'));
+	headerInnerStyle.appendChild(document.createTextNode('.Mat-mainWrapper {position: relative;}'));
 	document.head.appendChild(headerInnerStyle);
-	var matrixMainHolder = document.createElement('div');
-	matrixMainHolder.id = 'MatrixBgHolder';
-	matrixMainHolder.style.position = "absolute";
-	matrixMainHolder.style.top = "0";
-	matrixMainHolder.style.right = "0";
-	matrixMainHolder.style.bottom = "0";
-	matrixMainHolder.style.left = "0";
+	var matMainHolder = document.createElement('div');
+	matMainHolder.id = 'MatHolder';
+	matMainHolder.style.position = "absolute";
+	matMainHolder.style.top = "0";
+	matMainHolder.style.right = "0";
+	matMainHolder.style.bottom = "0";
+	matMainHolder.style.left = "0";
 	document.body.style.background = "#000";
-	var matrixMainCanvas = document.createElement('canvas');
-	matrixMainCanvas.id = 'MatrixCanvas';
-	var matrixMainKeypressAudio = document.createElement('audio');
-	matrixMainKeypressAudio.id = 'MatrixKeypress';
-	matrixMainKeypressAudio.src = dir + 'audio/keypress.mp3';
-	matrixMainKeypressAudio.setAttribute('preload', 'auto');
-	var MatrixModem = document.createElement('audio');
-	MatrixModem.id = 'MatrixModem';
-	MatrixModem.src = dir + 'audio/modem.mp3';
-	MatrixModem.setAttribute('preload', 'auto');
-	matrixMainHolder.appendChild(matrixMainCanvas);
-	matrixMainHolder.appendChild(matrixMainKeypressAudio);
-	matrixMainHolder.appendChild(MatrixModem);
-	document.body.insertBefore(matrixMainHolder, document.body.firstChild);
-	if(introParam == 'yes') {
- 		cursorMatrixBG(introParam);
+	var matMainCanvas = document.createElement('canvas');
+	matMainCanvas.id = 'MatCanvas';
+	var matMainKeypressAudio = document.createElement('audio');
+	matMainKeypressAudio.id = 'MatKeypress';
+	matMainKeypressAudio.src = dir + 'audio/keypressSound.mp3';
+	matMainKeypressAudio.setAttribute('preload', 'auto');
+	var MatModem = document.createElement('audio');
+	MatModem.id = 'MatModem';
+	MatModem.src = dir + 'audio/modemSound.mp3';
+	MatModem.setAttribute('preload', 'auto');
+	matMainHolder.appendChild(matMainCanvas);
+	matMainHolder.appendChild(matMainKeypressAudio);
+	matMainHolder.appendChild(MatModem);
+	document.body.insertBefore(matMainHolder, document.body.firstChild);
+	if(intParam == 'yes') {
+ 		cursorMatrixBG(intParam);
  		reloadDweetFor();
- 	} else if(introParam == 'write') {
+ 	} else if(intParam == 'write') {
  		cursorMatrixBG();
  		getUrlValue();
 		reloadDweetSend();
- 	} else if(introParam == 'read') {
+ 	} else if(intParam == 'read') {
  		cursorMatrixBG();
  		getUrlValue();
  		reloadDweetFor();
@@ -57,73 +57,74 @@ window.onload = function() {
  		getUrlValue();
  		reloadDweetFor();
  	}
+ 	changesThing();
  };
 
- function MatrixInit(intro)
+ function MatInit(intro)
  {
- 	introParam = intro;
+ 	intParam = intro;
  	
  }
 
-function setupMatrixBG()
+function setupMatBG()
 {
-	if(!canvasLoaded)
+	if(!canvasLoad)
 	{
-		canvas = document.getElementById("MatrixCanvas");
+		canvas = document.getElementById("MatCanvas");
 		ctx = canvas.getContext("2d");
 		letters = letters.split("");
-		canvasLoaded = true;
+		canvasLoad = true;
 	}
 	canvas.height = window.innerHeight;
 	canvas.width = window.innerWidth;
 	columns = canvas.width/fontSize; 
 	for(var x = 0; x < columns; x++){
-		letterPos[x] = window.innerHeight; 
+		lettersPos[x] = window.innerHeight; 
 	}
 }
 
 function cursorMatrixBG(introNeeded)
 {
-	setupMatrixBG();
+	setupMatBG();
 	var loadIntro = introNeeded || 'no';
 	if(loadIntro == 'yes')
 	{
 		var animText = new Array("$ root login:", "_", "neo", "$ root password:", "***","$ calling, please wait...", " OK", "$ entering the matrix, welcome.");
-		soundKeypress = document.getElementById('MatrixKeypress');
-		soundModem = document.getElementById('MatrixModem');
+		sdKeypress = document.getElementById('MatKeypress');
+		sdModem = document.getElementById('MatModem');
 		ctx.font = fontSize + "px arial";
 		ctx.fillStyle = '#0F0';
 	    ctx.font = '20px sans-serif';
 		ctx.fillText(animText[0], 20, fontSize);
 		ctx.fillText(animText[1], 130, fontSize);
-		timeouts.push(setTimeout(function(){ctx.clearRect(130, fontSize, canvas.width,fontSize);},timeoutParam));
-		timeouts.push(setTimeout(function(){ctx.fillText(animText[1], 130, fontSize);},timeoutParam*2));
-		timeouts.push(setTimeout(function(){ctx.clearRect(125, fontSize, canvas.width,fontSize);},timeoutParam*3));
-		timeouts.push(setTimeout(function(){ctx.fillText(animText[1], 130, fontSize);},timeoutParam*4));
-		timeouts.push(setTimeout(function(){ctx.clearRect(125, fontSize, canvas.width,fontSize);},timeoutParam*5));
-		timeouts.push(setTimeout(function(){ctx.fillText(animText[1], 130, fontSize);},timeoutParam*6));
-		timeouts.push(setTimeout(function(){ctx.clearRect(125, fontSize, canvas.width,fontSize);},timeoutParam*7));
-		timeouts.push(setTimeout(function(){typeOut(animText[2], 130, fontSize, 0, 0 ); soundKeypress.play();},timeoutParam*8));
-		timeouts.push(setTimeout(function(){ctx.fillText(animText[3], 20, fontSize*2);},timeoutParam*10));
-		timeouts.push(setTimeout(function(){ctx.fillText(animText[1], 175, fontSize*2);},timeoutParam*11));
-		timeouts.push(setTimeout(function(){ctx.clearRect(170, fontSize*2, canvas.width,fontSize);},timeoutParam*12));
-		timeouts.push(setTimeout(function(){typeOut(animText[4], 175, fontSize*2, 0, 0 ); soundKeypress.play();},timeoutParam*15));
-		timeouts.push(setTimeout(function(){ctx.fillText(animText[5], 20, fontSize*3);},timeoutParam*17));
-		timeouts.push(setTimeout(function(){soundModem.play();},timeoutParam*18));
-		timeouts.push(setTimeout(function(){ctx.fillText(animText[6], 230, fontSize*3);},timeoutParam*33));
-		timeouts.push(setTimeout(function(){ctx.clearRect(0, 0, canvas.width, canvas.height);},timeoutParam*35));
+		timeouts.push(setTimeout(function(){ctx.clearRect(130, fontSize, canvas.width,fontSize);},timeoutPar));
+		timeouts.push(setTimeout(function(){ctx.fillText(animText[1], 130, fontSize);},timeoutPar*2));
+		timeouts.push(setTimeout(function(){ctx.clearRect(125, fontSize, canvas.width,fontSize);},timeoutPar*3));
+		timeouts.push(setTimeout(function(){ctx.fillText(animText[1], 130, fontSize);},timeoutPar*4));
+		timeouts.push(setTimeout(function(){ctx.clearRect(125, fontSize, canvas.width,fontSize);},timeoutPar*5));
+		timeouts.push(setTimeout(function(){ctx.fillText(animText[1], 130, fontSize);},timeoutPar*6));
+		timeouts.push(setTimeout(function(){ctx.clearRect(125, fontSize, canvas.width,fontSize);},timeoutPar*7));
+		timeouts.push(setTimeout(function(){typeOut(animText[2], 130, fontSize, 0, 0 ); sdKeypress.play();},timeoutPar*8));
+		timeouts.push(setTimeout(function(){ctx.fillText(animText[3], 20, fontSize*2);},timeoutPar*10));
+		timeouts.push(setTimeout(function(){ctx.fillText(animText[1], 175, fontSize*2);},timeoutPar*11));
+		timeouts.push(setTimeout(function(){ctx.clearRect(170, fontSize*2, canvas.width,fontSize);},timeoutPar*12));
+		timeouts.push(setTimeout(function(){typeOut(animText[4], 175, fontSize*2, 0, 0 ); sdKeypress.play();},timeoutPar*15));
+		timeouts.push(setTimeout(function(){ctx.fillText(animText[5], 20, fontSize*3);},timeoutPar*17));
+		timeouts.push(setTimeout(function(){sdModem.play();},timeoutPar*18));
+		timeouts.push(setTimeout(function(){ctx.fillText(animText[6], 230, fontSize*3);},timeoutPar*33));
+		timeouts.push(setTimeout(function(){ctx.clearRect(0, 0, canvas.width, canvas.height);},timeoutPar*35));
 		timeouts.push(setTimeout(function(){
-			mainInterval = setInterval(drawMatrixBG, 80); 
-			fadeIn(MatrixMainWrapper); 
-			bodyLoaded=true;
-		},(timeoutParam*35)+100));
+			mainInt = setInterval(drawMatrixBG, 80); 
+			fadeIn(matMainWrapper); 
+			bodyLoad=true;
+		},(timeoutPar*35)+100));
 	}
 	else
 	{
-		mainInterval = setInterval(drawMatrixBG, 80); 
-		if(!bodyLoaded){
-				fadeIn(MatrixMainWrapper); 
-				bodyLoaded=true;
+		mainInt = setInterval(drawMatrixBG, 80); 
+		if(!bodyLoad){
+				fadeIn(matMainWrapper); 
+				bodyLoad=true;
 			}
 	}
 }
@@ -136,25 +137,25 @@ function drawMatrixBG()
 
 	ctx.fillStyle = "#fff"; 
 	ctx.font = fontSize + "px matrixFont";
-	for(var i = 0; i < letterPos.length; i++)
+	for(var i = 0; i < lettersPos.length; i++)
 	{
 		var text = letters[Math.floor(Math.random()*letters.length)];
 			prev.push(text);
-			ctx.fillText(text, i*fontSize, letterPos[i]*fontSize);
+			ctx.fillText(text, i*fontSize, lettersPos[i]*fontSize);
 
-		if(letterPos[i]*fontSize > canvas.height && Math.random() > 0.945){
-			letterPos[i] = 0;
+		if(lettersPos[i]*fontSize > canvas.height && Math.random() > 0.945){
+			lettersPos[i] = 0;
 		}
-		letterPos[i]++;
+		lettersPos[i]++;
 	}
-	whiteFontInterval = setTimeout(function(){ 
+	whiteFontInt = setTimeout(function(){ 
 		ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
 		ctx.fillStyle = "#0F0"; 
 		ctx.font = fontSize + "px matrixFont";
-		for(var r = 0; r < letterPos.length; r++)
+		for(var r = 0; r < lettersPos.length; r++)
 		{
-			ctx.fillText(prev[r], r*fontSize, (letterPos[r]-1)*fontSize);
+			ctx.fillText(prev[r], r*fontSize, (lettersPos[r]-1)*fontSize);
 		}
 	}, 190);
 }
@@ -216,7 +217,8 @@ function dweetMessage() {
 		1) add lines, date, number of rotate, text after rotate and text of array end.
 		2) array concatenation.
 */
-	var na = ["--------------"];								// 
+	var na = [];
+		na.push("--------------");								// 
 		na.push("Date de la séquence " + dateString );
 		na.push("--------------");
 		na.push("utilisez ROT" + numberRotate + " après assemblage");
@@ -285,7 +287,7 @@ function typeOut(str, startX, startY, lineHeight, padding) {
         if(i === str.length) {
             clearInterval($_inter);
         }
-    }, keypressInterval);
+    }, kpressInt);
 }
 
 function fadeIn(mainElement){
@@ -300,26 +302,26 @@ function fadeIn(mainElement){
   })();
 }
 function onCanvasResize() {
-	if(soundKeypress)
+	if(sdKeypress)
 	{
-		soundKeypress.pause();
-		soundKeypress.currentTime = 0;	
+		sdKeypress.pause();
+		sdKeypress.currentTime = 0;	
 	}
-	if(soundModem)
+	if(sdModem)
 	{
-		soundModem.pause();
-		soundModem.currentTime = 0;
+		sdModem.pause();
+		sdModem.currentTime = 0;
 	}
-    clearInterval(mainInterval);
-	clearInterval(whiteFontInterval);
+    clearInterval(mainInt);
+	clearInterval(whiteFontInt);
 	for (var i=0; i<timeouts.length; i++) {
 	  clearTimeout(timeouts[i]);
 	}
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	cursorMatrixBG();
-	if(!bodyLoaded){
-		fadeIn(MatrixMainWrapper);
-		bodyLoaded = true;
+	if(!bodyLoad){
+		fadeIn(matMainWrapper);
+		bodyLoad = true;
 	}
 }
 // Read a page's GET URL variables and return them as an associative array.
@@ -381,6 +383,23 @@ function addUrlParam(url, key, value) {
     }
     return result;
 }
+function changesThing()
+{
+	var thing = document.querySelector('#thing');
+	var title = document.querySelector('#title');
+	thing.addEventListener('thing', function()
+	{
+    title.textContent = thing.value;
+    var addThing = thing.value;
+	if(intParam == 'yes' || intParam == 'read') {
+ 		reloadDweetFor();
+ 	} else if(intParam == 'write') {
+		reloadDweetSend();
+ 	} else {
+ 		reloadDweetFor();
+ 	}
+	});
+}
 function reloadDweetFor()
 {
 	var txr = "<br>hello, concerning readDweet<br>push the rotate button or Access key 'r'<br>push the write button or Access key 'e'<br>push the reload button after modify thing or Access key 'd'";
@@ -390,7 +409,7 @@ function reloadDweetFor()
 		document.getElementById('title').innerHTML = addThing;
 		document.getElementById('rotR').innerHTML = null;
 		document.getElementById('rotR').innerHTML =  txr + akw;
-	if(introParam == 'yes') {var dir = ''} else {var dir = '../'}
+	if(intParam == 'yes') {var dir = ''} else {var dir = '../'}
 	var url = "location.href='"+dir+"write/?r=" + numberRotate + "&p=1'";
 		link.setAttribute('onclick', addUrlParam(url, "t", addThing));
 		getDweetFor();
@@ -404,7 +423,7 @@ function reloadDweetSend()
 		document.getElementById('title').innerHTML = addThing;
 		document.getElementById('rotS').innerHTML = null;
 		document.getElementById('rotS').innerHTML =  txw + akw;
-	if(introParam == 'yes') {var dir = ''} else {var dir = '../'}
+	if(intParam == 'yes') {var dir = ''} else {var dir = '../'}
 	var url = "location.href='"+dir+"read/?r=" + numberRotate + "&p=1'";
 		link.setAttribute('onclick', addUrlParam(url, "t", addThing));
 		document.getElementById('sendProgress').style.display = 'none';
